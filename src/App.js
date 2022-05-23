1abf1620bfa386bff8e4ec014b1fd73fda06747c
hossamdebyene <79216694+hossamdebyene@users.noreply.github.com>
2022-05-23 12:58:31 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import { tokening, artist } from "./actions";
 import './App.css';
 import AppRouter from './AppRouter'
 function App() {
 
-
+  const dispatch = useDispatch()
+  dispatch(tokening(""))
+  dispatch(artist({ artists: [], searchKey: "" }))
   return (
     <Router>
-    <div className="App">
-      <div className='header'>
-        <span>Spotify Artist Search</span>
+      <div className="App">
+        <div className='header'>
+          <span>Spotify Artist Search</span>
+        </div>
+        <div className='content'>
+          <AppRouter />
+        </div>
       </div>
-      <div className='content'>
-        <AppRouter />
-      </div>
-    </div>
     </Router>
   );
 }
