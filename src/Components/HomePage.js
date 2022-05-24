2a9d53685d49ad4672b10218dad3153f44e8bc37
hossamdebyene <79216694+hossamdebyene@users.noreply.github.com>
2022-05-24 16:24:08 +0300

    FIXES

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -27,25 +27,24 @@ function HomePage() {
     const navigate = useNavigate()
 
     // to track the value of state
-    const [searchKey, setSearchKey] = useState( Artistss.searchKey )
+    const [searchKey, setSearchKey] = useState(Artistss.searchKey)
     const [artists, setArtists] = useState(Artistss.artists)
 
 
     // to rerender function after authentication
     useEffect(() => {
-    const hash = location.hash
-    let token = tokenss
+        const hash = location.hash
+        let token = tokenss
 
-    if (!tokenss && hash) {
-        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+        if (!tokenss && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-        window.location.hash = ""
-        dispatch(tokening(token))
-    }
+            location.hash = ""
+            dispatch(tokening(token))
+        }
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [tokenss])
-
 
     // to get data of the searched Artists
     const searchArtists = useCallback(async () => {
@@ -60,20 +59,28 @@ function HomePage() {
                 type: "artist"
             }
         }).catch(function (error) {
-            if (error.response.status === 401) {
+            if (error.response.status === 400) {
                 dispatch(tokening(""))
                 navigate("/")
             }
-          }).then(function (response) {
+            else if (error.response.status === 401) {
+                dispatch(tokening(""))
+                navigate("/")
+            }
+            else if (error.response.status === 403) {
+                dispatch(tokening(""))
+                navigate("/")
+            }
+        }).then(function (response) {
             setArtists(response.data.artists.items)
-          });
+        });
         //setting artists with the response of the get api
         // if(data.status === 200){
         //     
         // }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [searchKey, tokenss])
-    
+
 
     //requesting get api with artist id for getting artist albums
     //updating redux artist state with the searchkey and artists to retrieve when going back
@@ -86,71 +93,60 @@ function HomePage() {
                 Authorization: `Bearer ${tokenss}`
             },
         }).catch(function (error) {
-            if (error.response.status === 401) {
+            if (error.response.status >= 400) {
                 dispatch(tokening(""))
                 navigate("/")
             }
-          }).then(function (response) {
-            dispatch(artist({ artists:artists, searchKey: searchKey}))
-            navigate('/ArtistAlbums', {state: {artistAlbums:response.data.items, artist:artists }})
-          });
+        }).then(function (response) {
+            dispatch(artist({ artists: artists, searchKey: searchKey }))
+            navigate('/ArtistAlbums', { state: { artistAlbums: response.data.items, artist: artists } })
+        });
 
 
     }
 
     //to handle key when typing in order to implement search as type function
     const keyUp = useCallback(() => {
-        if(searchKey === ""){
+        if (searchKey === "") {
             setArtists([])
         }
-        else{
-            searchArtists ()
-            const data  = axios.get("https://api.spotify.com/v1/search", {
-                headers: {
-                    Authorization: `Bearer ${tokenss}`
-                },
-                params: {
-                    q: searchKey,
-                    type: "artist"
-                }
-            })
-            console.log(data)
-
+        else {
+            searchArtists()
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [searchArtists, searchKey])
-    
+
 
     // to rerender every keyup method to check with every change in searchkey or artists or keyUp with timeout 
     //timeout is to render the function after 1s of untyping
     useEffect(() => {
         const timeoutId = setTimeout(() => keyUp(), 1000);
         return () => clearTimeout(timeoutId);
-      }, [searchKey, artists, keyUp]);
+    }, [searchKey, artists, keyUp]);
 
     return (
-            <div>
-                <form onSubmit={searchArtists} className="middle">
-                    <input className="type" value={searchKey} autoFocus placeholder='Search for an artist ...' type="text" onKeyUp={(e) => keyUp()} onChange={(e) => setSearchKey(e.target.value)} />
-                    <button className='but' type={"submit"}><AiOutlineSearch /></button>
-                </form>
-                <div className="main row" >
-                    {artists.length > 0 ? artists.map(artist => (
-                        <div className="artistCard mb-3 p-0" key={artist.id} type="button" onClick={(e) => artistAlbum(e,artist)}>
-                            <Card >
-                                <Card.Img className="cardimg" variant="top" src={artist.images.length ? artist.images[0].url : defaultImage} />
-                                <Card.Body>
-                                    <Card.Title className="tit">{artist.name}</Card.Title>
-                                    <Card.Text className='sub'>
-                                        {(artist.followers.total).toLocaleString("en")} followers
-                                    </Card.Text>
-                                    <Rate className='stars' disabled defaultValue={(artist.popularity / 100 * 5)} />
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    )):null}
-                </div>
+        <div>
+            <form onSubmit={searchArtists} className="middle">
+                <input className="type" value={searchKey} autoFocus placeholder='Search for an artist ...' type="text" onKeyUp={(e) => keyUp()} onChange={(e) => setSearchKey(e.target.value)} />
+                <button className='but' type={"submit"}><AiOutlineSearch /></button>
+            </form>
+            <div className="main row" >
+                {artists.length > 0 ? artists.map(artist => (
+                    <div className="artistCard mb-3 p-0" key={artist.id} type="button" onClick={(e) => artistAlbum(e, artist)}>
+                        <Card >
+                            <Card.Img className="cardimg" variant="top" src={artist.images.length ? artist.images[0].url : defaultImage} />
+                            <Card.Body>
+                                <Card.Title className="tit">{artist.name}</Card.Title>
+                                <Card.Text className='sub'>
+                                    {(artist.followers.total).toLocaleString("en")} followers
+                                </Card.Text>
+                                <Rate className='stars' disabled defaultValue={(artist.popularity / 100 * 5)} />
+                            </Card.Body>
+                        </Card>
+                    </div>
+                )) : null}
             </div>
+        </div>
     )
 }
 
