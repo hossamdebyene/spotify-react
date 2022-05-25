373a354fba77d5748ef7152956a8f13e9b07c55f
hossamdebyene <79216694+hossamdebyene@users.noreply.github.com>
2022-05-25 12:23:16 +0300

    UPDATE

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,10 +7,10 @@ import { tokening, artist } from "../actions"
 import { useLocation, useNavigate } from "react-router-dom";
 
 
-function Login () {
+function Login() {
 
     //Spotify Credentials from spotify app
-    const CLIENT_ID = "8c1d66b00a004f9b98cf287ab9cfe3ba"
+    const CLIENT_ID = "c759e373438a4a2d8611895b8fc7fdb3"
     const REDIRECT_URI = "http://localhost:3000/ArtistSearch"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
@@ -21,38 +21,38 @@ function Login () {
     const dispatch = useDispatch()
     // for getting token state from redux
     const tokenss = useSelector((state) => state.tokening)
-    
-    
+
+
     //for declaring the redux artist state
-    dispatch(artist({artists:[], searchKey:""}))
+    dispatch(artist({ artists: [], searchKey: "" }))
 
     //to navigate between pages
     const navigate = useNavigate()
 
     //to rerender on tokenss value change
     useEffect(() => {
-    const hash = location.hash
-    let token = tokenss
+        const hash = location.hash
+        let token = tokenss
 
-    // getToken()
-    if(tokenss){
-        navigate('/ArtistSearch')
-    }
+        // getToken()
+        if (tokenss) {
+            navigate('/ArtistSearch')
+        }
 
-    if (!tokenss && hash) {
-        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+        if (!tokenss && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-        location.hash = ""
-        dispatch(tokening(token))
-    }
+            location.hash = ""
+            dispatch(tokening(token))
+        }
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-}, [tokenss])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [tokenss])
 
-return(
-    <div className='center'>
-        <a className='middle' href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}><p className='buttonName'>Login</p><img className='logo' src={Logo} alt="Woocomerce" /> </a>
-    </div>
-)
+    return (
+        <div className='center'>
+            <a className='middle' href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}><p className='buttonName'>Login</p><img className='logo' src={Logo} alt="Woocomerce" /> </a>
+        </div>
+    )
 }
 export default Login
